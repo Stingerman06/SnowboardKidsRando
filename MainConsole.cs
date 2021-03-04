051af1a3cadbf34dae060fc7174969889ea6909a
Curry <curry_dockins@yahoo.com>
2021-03-03 23:43:12 -0500

    Update MainConsole.cs
    Gave functionality to the code. Can read .n64/.v64/z64 ROMs properly. Reads user input in multiple spots. No RAND functionality just yet, but will add it later. Some spots that reads key will be taken out next update. Or not. Depends how it feels on flow.

--- MainConsole.cs
+++ MainConsole.cs
@@ -15,50 +15,178 @@ namespace SnowboardKidsRandomizer
     {
         static void Main(string[] args)
         {
-            string endianType = "None";
-            Console.WriteLine("Please specify the name of the ROM you wish to use with the file extension.\n\nNotice the ROM must be in the same directory as this program,\nand must be named 'Snowboard Kids(USA)' without the quotations.\nYou must use the USA version and not the Japanese Version of the game.\nCase Sensitive. Extension does not matter. Type EXIT to close this program.");
-            Retry:
-            string gameName = Console.ReadLine();
-            if(gameName == "Snowboard Kids(USA).n64" || gameName == "Snowboard Kids(USA).N64")
+        RETRY:
+            string date = string.Format("{0:yyyyMMdd}", DateTime.Now);
+            string endianType = "";
+            Console.WriteLine("Please specify the name of the ROM you wish to use with the file extension.\n\nNotice the ROM must be in the same directory as this program,\nand must be named 'Snowboard Kids(USA)' without the quotations.\nYou must use the USA version and not the Japanese Version of the game.\nCase Sensitive. Extension does not matter. Type EXIT to close this program.\n");
+            
+            string gameName = Console.ReadLine();       //Check user input for ROM. Don't know how dragging and dropping works with coding atm...
+
+            if(gameName == "Snowboard Kids(USA).v64" || gameName == "Snowboard Kids(USA).V64")
             {
-                endianType = "SWAP";
+                endianType = "V64";
+                goto ROMREAD;
             }
-            /*else if(gameName == "Snowboard Kids(USA).v64" || gameName == "Snowboard Kids(USA).V64")
+
+            else if(gameName == "Snowboard Kids(USA).n64" || gameName == "Snowboard Kids(USA).N64")
             {
-                endianType = "BIG";
+                endianType = "N64";
+                goto ROMREAD;
             }
+
             else if (gameName == "Snowboard Kids(USA).z64" || gameName == "Snowboard Kids(USA).Z64")
             {
-                endianType = "SMOL";
-            }*/
+                endianType = "Z64";
+                goto ROMREAD;
+            }
+
             else if (gameName == "EXIT")
             {
                 goto EXIT;
             }
-            else
-            {
-                Console.WriteLine("Name does not match properly. Be sure to type the name correctly and try again. Case sensitive...");
-                goto Retry;
-            }
-            FileStream ROM = new FileStream(gameName, FileMode.Open, FileAccess.Read);
-            if (endianType == "SWAP")
-            {
-            }
-            /*else if (endianType == "BIG")
-            {
-            }
-            else if (endianType == "SMOL")
-            {
-            }*/
 
             else
             {
-                Console.WriteLine("I don't know how you got access to this text, but something messed up in the code.\nIf you see this, panic and contact the developer of this program for debugging.\nThe program will now close. Nothing will be saved.");
-                //F.Close();
-                //goto EXIT;
+                Console.WriteLine("Name does not match properly. Be sure to type the name correctly and try again. Case sensitive...\n");
+                goto RETRY;
             }
+
+        ROMREAD:                                //Checks for the ROM's true Endian Nature and if the user is lying/stupid.
+
+            if (!File.Exists(gameName))
+            {
+                Console.WriteLine("The ROM with the specified name does not exist. Make sure the ROM is in\nthe same folder as this program and try again.");
+                goto EXIT;
+            }
+            FileStream ROM = new FileStream(gameName, FileMode.Open, FileAccess.Read);
+
+            int bytesToRead = 0x02;
+            int bytesRead = 0x00;
+            byte[] singleByte = new byte[bytesToRead];
+            while (bytesToRead > 0)
+            {
+            int readByte = ROM.Read(singleByte, bytesRead, bytesToRead);
+                if (readByte == 0)
+                    break;
+                bytesRead += readByte;
+                bytesToRead -= readByte;
+            }
+
+            Array.Reverse(singleByte);
+            int singleInt = BitConverter.ToInt16(singleByte);
+            Console.WriteLine(singleInt);
+            Console.ReadKey();
+
+            if (singleInt == 0x4012)
+            {
+                if (endianType == "N64")
+                {
+                    Console.WriteLine("This ROM is wordswapped, and commonly uses .n64/.N64\n");
+                    goto N64;
+                }
+                else if (endianType == "V64")
+                {
+                    Console.WriteLine("This ROM extension is not .n64/.N64, but uses the same data format.\nROM will be output as .n64.");
+                    goto N64;
+                }
+                else if (endianType == "Z64")
+                {
+                    Console.WriteLine("This ROM extension is not .n64/.N64, but uses the same data format.\nROM will be output as .n64.");
+                    goto N64;
+                }
+            }
+                
+            else if(singleInt == 0x3780)
+            {
+                if (endianType == "N64")
+                {
+                    Console.WriteLine("This ROM extension is not .v64/.V64, but uses the same data format.\nROM will be output as .v64.");
+                    goto V64;
+                }
+                else if (endianType == "V64")
+                {
+                    Console.WriteLine("This ROM is byteswapped, and commonly uses .v64/.V64\n");
+                    goto V64;
+                }
+                else if (endianType == "Z64")
+                {
+                    Console.WriteLine("This ROM extension is not .v64/.V64, but uses the same data format.\nROM will be output as .v64.");
+                    goto V64;
+                }
+            }
+
+            else if (singleInt == 0x8037)
+            {
+                if (endianType == "N64")
+                {
+                    Console.WriteLine("This ROM extension is not .z64/.Z64, but uses the same data format.\nROM will be output as .z64.");
+                    goto Z64;
+                }
+                else if (endianType == "V64")
+                {
+                    Console.WriteLine("This ROM extension is not .v64/.V64, but uses the same data format.\nROM will be output as .z64.");
+                    goto Z64;
+                }
+                else if (endianType == "Z64")
+                {
+                    Console.WriteLine("This ROM has no bytes swapped around, and commonly uses .z64/.Z64\n");
+                    goto Z64;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("This ROM is either wordswapped and byteswapped, or this is not a proper Nintendo 64 ROM.\nPress any key to exit.");
+                goto EXIT;
+            }
+
+        N64:                //Do stuff here if .N64/wordswapped.
+
+            Console.ReadKey();
+            Console.WriteLine("Creating a new ROM file based on ROM file given...");
+            FileStream newN64ROM = new FileStream("Snowboard Kids(USA)" + date + ".n64", FileMode.CreateNew, FileAccess.ReadWrite);
+            newN64ROM.Position = 0x3F;
+            {
+                Console.WriteLine("Writing bytes to new ROM...");
+                newN64ROM.WriteByte(0x40);
+            }
+            Console.WriteLine("Finishing up...");
+            newN64ROM.Close();
+            goto CLOSE;
+
+        V64:                //Do stuff here if .V64/byteswapped.
+
+            Console.ReadKey();
+            Console.WriteLine("Creating a new ROM file based on ROM file given...");
+            FileStream newV64ROM = new FileStream("Snowboard Kids(USA)" + date + ".v64", FileMode.CreateNew, FileAccess.ReadWrite);
+
+            newV64ROM.Position = 0x36;      //Temporarily writes a single byte and just closes. Nothing necessary quite yet.
+            {
+                Console.WriteLine("Writing bytes to new ROM...");
+                newV64ROM.WriteByte(0x37);
+            }
+            Console.WriteLine("Finishing up...");
+            newV64ROM.Close();
+            goto CLOSE;
+
+        Z64:                //Do stuff here if .Z64/no swapped bytes.
+
+            Console.ReadKey();
+            Console.WriteLine("Creating a new ROM file based on ROM file given...");
+            FileStream newZ64ROM = new FileStream("Snowboard Kids(USA)" + date + ".z64", FileMode.CreateNew, FileAccess.ReadWrite);
+            newZ64ROM.Position = 0x7F;
+            {
+                Console.WriteLine("Writing bytes to new ROM...");
+                newZ64ROM.WriteByte(0x80);
+            }
+            Console.WriteLine("Finishing up...");
+            newZ64ROM.Close();
+            goto CLOSE;
+
+        CLOSE:
             ROM.Close();
-            EXIT:
+        EXIT:
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
     }
